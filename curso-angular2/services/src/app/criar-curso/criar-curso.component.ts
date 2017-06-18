89b02eac0e51fc72263b019ac5d5215d9e0cbcdd
Danilo C. Graciano <danilocgraciano@gmail.com>
2017-06-18 16:22:34 -0300

    Services
    Escopo de instancia e singleton

---
+++ curso-angular2/services/src/app/criar-curso/criar-curso.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CursosService } from "app/cursos/cursos.service";
+
+@Component({
+  selector: 'app-criar-curso',
+  templateUrl: './criar-curso.component.html',
+  styleUrls: ['./criar-curso.component.css']
+  // ,providers: [CursosService]//escopo de instancia
+})
+export class CriarCursoComponent implements OnInit {
+
+  cursos: string[] = [];
+
+  constructor(private service: CursosService) { }
+
+  ngOnInit() {
+    this.cursos = this.service.getCursos();
+  }
+
+  onAddCurso(curso: string) {
+    this.service.addCurso(curso);
+  }
+
+}
--- curso-angular2/services/src/app/cursos/cursos.service.ts
+++ curso-angular2/services/src/app/cursos/cursos.service.ts
@@ -3,7 +3,17 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class CursosService {
 
+    private cursos: string[] = ['Angular 2', 'Java', 'PhoneGap'];
+
+    constructor() {
+        console.log('CursosService.constructor()');
+    }
+
     getCursos(): string[] {
-        return ['Angular 2', 'Java', 'PhoneGap'];
+        return this.cursos;
+    }
+
+    addCurso(curso: string): void {
+        this.cursos.push(curso);
     }
 }
