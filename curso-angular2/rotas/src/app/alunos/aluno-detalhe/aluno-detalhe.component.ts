b892309737fdfb9e5a34db069ea75f4eb1e7af07
Danilo C. Graciano <danilocgraciano@gmail.com>
2017-07-16 16:03:49 -0300

    Rotas Filhas
    Segunda Parte

--- curso-angular2/rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
+++ curso-angular2/rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { Subscription } from "rxjs/Rx";
+import { AlunosService } from "app/alunos/alunos.service";
 
 @Component({
   selector: 'app-aluno-detalhe',
   templateUrl: './aluno-detalhe.component.html',
   styleUrls: ['./aluno-detalhe.component.css']
 })
-export class AlunoDetalheComponent implements OnInit {
+export class AlunoDetalheComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  id: number;
+  inscricao: Subscription;
+  aluno: any;
+
+  constructor(private route: ActivatedRoute, private router: Router, private alunosService: AlunosService) { }
 
   ngOnInit() {
+    this.inscricao = this.route.params.subscribe((params: any) => {
+      this.id = params['id'];
+      this.aluno = this.alunosService.getAluno(this.id);
+    }
+    );
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
+
+  editarContato() {
+    this.router.navigate(['/alunos', this.aluno.id, 'editar']);
   }
 
 }
--- curso-angular2/rotas/src/app/alunos/aluno-form/aluno-form.component.ts
+++ curso-angular2/rotas/src/app/alunos/aluno-form/aluno-form.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { Subscription } from "rxjs/Rx";
+import { AlunosService } from "app/alunos/alunos.service";
 
 @Component({
   selector: 'app-aluno-form',
   templateUrl: './aluno-form.component.html',
   styleUrls: ['./aluno-form.component.css']
 })
-export class AlunoFormComponent implements OnInit {
+export class AlunoFormComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  id: number;
+  inscricao: Subscription;
+  aluno: any;
+
+  constructor(private route: ActivatedRoute, private alunosService: AlunosService) { }
 
   ngOnInit() {
+    this.inscricao = this.route.params.subscribe((params: any) => {
+      this.id = params['id'];
+      this.aluno = this.alunosService.getAluno(this.id);
+
+      if (this.aluno == null) {
+        this.aluno = {};
+      }
+    }
+    );
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 
 }
+
